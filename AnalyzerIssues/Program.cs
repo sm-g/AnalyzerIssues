55296ac70e541c1ca6bb24258779cc7a575dfa2f
smg <semagerasimov@gmail.com>
2019-11-05 15:50:09 +0300

    Add project files.

---
+++ AnalyzerIssues/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AnalyzerIssues
+{
+    using System;
+
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello world!");
+        }
+    }
+}
---
+++ AnalyzerIssues/S1854.cs
@@ -0,0 +1,54 @@
+﻿namespace AnalyzerIssues
+{
+    using System;
+
+    internal class S1854
+    {
+        public int FalsePositive()
+        {
+            var name = string.Empty;
+            try
+            {
+                var values = GetValues();
+#pragma warning disable S1854 // Dead stores should be removed
+                name = values.name;
+#pragma warning restore S1854 // Dead stores should be removed
+
+                DoWork();
+                return 5;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{name}: {e}");
+                throw;
+            }
+        }
+
+        public void ThisIsOk()
+        {
+            var name = string.Empty;
+            try
+            {
+                var values = GetValues();
+                name = values.name;
+
+                DoWork();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{name}: {e}");
+                throw;
+            }
+        }
+
+        private (string name, int count) GetValues()
+        {
+            return ("foo", 1);
+        }
+
+        private void DoWork()
+        {
+            throw new InvalidOperationException("bang");
+        }
+    }
+}
